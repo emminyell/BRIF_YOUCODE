f4bd4d378747b6649cf2c1858ec3dde66e8b5d7c
emminyell <140811695+emminyell@users.noreply.github.com>
2023-12-25 06:24:21 +0000

    validate sing up and login

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,16 +1,3 @@
 <?php
 
 require_once 'models/User.php';
-
-class UserController {
-  
-
-    public function listUsers() {
-        $user =new User();
-        $users = $user->getUsers();
-    }
-
-  
-    
-
-}
